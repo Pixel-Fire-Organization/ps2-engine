77884ec5ffd2669555b7a5df11843f30fe51e448
Boyan Vidolov <boyan.vidolov@gmail.com>
2026-04-04 23:03:19 +0300

    add: debug symbol generation & ELF emulator run stage

--- scripts/runEmulator.bat
+++ scripts/runEmulator.bat
@@ -1,13 +1,13 @@
 @echo off
 
-set "ISO_PATH=%~1"
+set "FILE_PATH=%~1"
 set "PCSX2_PATH=%~2"
 
-if "%ISO_PATH%"=="" set "ISO_PATH=.\exec\engine.iso"
+if "%FILE_PATH%"=="" set "FILE_PATH=.\exec\engine.iso"
 
-echo === Launching PCSX2 with %ISO_PATH% ===
+echo === Launching PCSX2 with %FILE_PATH% ===
 if "%PCSX2_PATH%"=="" (
-    powershell -ExecutionPolicy Bypass -File "%~dp0runEmulator.ps1" -IsoPath "%ISO_PATH%"
+    powershell -ExecutionPolicy Bypass -File "%~dp0runEmulator.ps1" -FilePath "%FILE_PATH%"
 ) else (
-    powershell -ExecutionPolicy Bypass -File "%~dp0runEmulator.ps1" -IsoPath "%ISO_PATH%" -Pcsx2Path "%PCSX2_PATH%"
+    powershell -ExecutionPolicy Bypass -File "%~dp0runEmulator.ps1" -FilePath "%FILE_PATH%" -Pcsx2Path "%PCSX2_PATH%"
 )
